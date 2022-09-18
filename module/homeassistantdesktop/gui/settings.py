753d3ea7cbafe49ef70cc89229b5f61ee442ef74
Aidan Timson <contact@timmo.xyz>
2022-09-19 00:11:26 +0100

    Alignment

--- module/homeassistantdesktop/gui/settings.py
+++ module/homeassistantdesktop/gui/settings.py
@@ -51,16 +51,20 @@ class GUISettings(QtWidgets.QWidget):
             else QtCore.Qt.CheckState.Unchecked
         )
         layout_autostart = QtWidgets.QHBoxLayout()
-        layout_autostart.addWidget(label_autostart)
-        layout_autostart.addWidget(self.input_autostart)
+        layout_autostart.addWidget(label_autostart, stretch=0)
+        layout_autostart.addWidget(
+            self.input_autostart, stretch=1, alignment=QtCore.Qt.AlignRight
+        )
 
         label_log_level = QtWidgets.QLabel("Log Level")
         label_log_level.setFont(QtGui.QFont("Roboto Regular", 24))
         self.input_log_level = QtWidgets.QLineEdit()
         self.input_log_level.setText(setting_log_level)
         layout_log_level = QtWidgets.QHBoxLayout()
-        layout_log_level.addWidget(label_log_level)
-        layout_log_level.addWidget(self.input_log_level)
+        layout_log_level.addWidget(label_log_level, stretch=0)
+        layout_log_level.addWidget(
+            self.input_log_level, stretch=1, alignment=QtCore.Qt.AlignRight
+        )
 
         label_home_assistant_secure = QtWidgets.QLabel("Home Assistant SSL")
         label_home_assistant_secure.setFont(QtGui.QFont("Roboto Regular", 24))
@@ -71,44 +75,52 @@ class GUISettings(QtWidgets.QWidget):
             else QtCore.Qt.CheckState.Unchecked
         )
         layout_home_assistant_secure = QtWidgets.QHBoxLayout()
-        layout_home_assistant_secure.addWidget(label_home_assistant_secure)
-        layout_home_assistant_secure.addWidget(self.input_home_assistant_secure)
+        layout_home_assistant_secure.addWidget(label_home_assistant_secure, stretch=0)
+        layout_home_assistant_secure.addWidget(
+            self.input_home_assistant_secure, stretch=1, alignment=QtCore.Qt.AlignRight
+        )
 
         label_home_assistant_host = QtWidgets.QLabel("Home Assistant Host")
         label_home_assistant_host.setFont(QtGui.QFont("Roboto Regular", 24))
         self.input_home_assistant_host = QtWidgets.QLineEdit()
         self.input_home_assistant_host.setText(setting_home_assistant_host)
         layout_home_assistant_host = QtWidgets.QHBoxLayout()
-        layout_home_assistant_host.addWidget(label_home_assistant_host)
-        layout_home_assistant_host.addWidget(self.input_home_assistant_host)
+        layout_home_assistant_host.addWidget(label_home_assistant_host, stretch=0)
+        layout_home_assistant_host.addWidget(
+            self.input_home_assistant_host, stretch=1, alignment=QtCore.Qt.AlignRight
+        )
 
         label_home_assistant_port = QtWidgets.QLabel("Home Assistant Port")
         label_home_assistant_port.setFont(QtGui.QFont("Roboto Regular", 24))
         self.input_home_assistant_port = QtWidgets.QLineEdit()
         self.input_home_assistant_port.setText(setting_home_assistant_port)
         layout_home_assistant_port = QtWidgets.QHBoxLayout()
-        layout_home_assistant_port.addWidget(label_home_assistant_port)
-        layout_home_assistant_port.addWidget(self.input_home_assistant_port)
+        layout_home_assistant_port.addWidget(label_home_assistant_port, stretch=0)
+        layout_home_assistant_port.addWidget(
+            self.input_home_assistant_port, stretch=1, alignment=QtCore.Qt.AlignRight
+        )
 
         label_home_assistant_token = QtWidgets.QLabel("Home Assistant Token")
         label_home_assistant_token.setFont(QtGui.QFont("Roboto Regular", 24))
         self.input_home_assistant_token = QtWidgets.QLineEdit()
         self.input_home_assistant_token.setText(secret_home_assistant_token)
         layout_home_assistant_token = QtWidgets.QHBoxLayout()
-        layout_home_assistant_token.addWidget(label_home_assistant_token)
-        layout_home_assistant_token.addWidget(self.input_home_assistant_token)
+        layout_home_assistant_token.addWidget(label_home_assistant_token, stretch=0)
+        layout_home_assistant_token.addWidget(
+            self.input_home_assistant_token, stretch=1, alignment=QtCore.Qt.AlignRight
+        )
 
         self.button_save = QtWidgets.QPushButton("Save")
 
         self.layout = QtWidgets.QVBoxLayout(self)  # type: ignore
-        self.layout.addWidget(label_heading, 0, QtCore.Qt.AlignTop)  # type: ignore
-        self.layout.addLayout(layout_autostart, 0)  # type: ignore
-        self.layout.addLayout(layout_log_level, 0)  # type: ignore
-        self.layout.addLayout(layout_home_assistant_secure, 0)  # type: ignore
-        self.layout.addLayout(layout_home_assistant_host, 0)  # type: ignore
-        self.layout.addLayout(layout_home_assistant_port, 0)  # type: ignore
-        self.layout.addLayout(layout_home_assistant_token, 0)  # type: ignore
-        self.layout.addWidget(self.button_save, 0, QtCore.Qt.AlignBottom)  # type: ignore
+        self.layout.addWidget(label_heading, stretch=0, alignment=QtCore.Qt.AlignTop)  # type: ignore
+        self.layout.addLayout(layout_autostart, stretch=0)  # type: ignore
+        self.layout.addLayout(layout_log_level, stretch=0)  # type: ignore
+        self.layout.addLayout(layout_home_assistant_secure, stretch=0)  # type: ignore
+        self.layout.addLayout(layout_home_assistant_host, stretch=0)  # type: ignore
+        self.layout.addLayout(layout_home_assistant_port, stretch=0)  # type: ignore
+        self.layout.addLayout(layout_home_assistant_token, stretch=0)  # type: ignore
+        self.layout.addWidget(self.button_save, stretch=0, alignment=QtCore.Qt.AlignBottom)  # type: ignore
 
         self.button_save.clicked.connect(self.save)  # type: ignore
 
