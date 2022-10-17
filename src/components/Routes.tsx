08e2a780c2e1799fce27211032b02d2d17a7af80
Aidan Timson <contact@timmo.xyz>
2022-10-17 16:03:25 +0100

    Remove unused header

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,14 +1,14 @@
 import { ReactElement } from "react";
 import { Container, Typography } from "@mui/material";
 
-import Header from "@/components/Header";
+// import Header from "@/components/Header";
 import Settings from "@/components/settings/Settings";
 
 const title = "Settings";
 function Routes(): ReactElement {
   return (
     <>
-      <Header title={title} />
+      {/* <Header title={title} /> */}
       <Container maxWidth="xl" sx={{ p: 2 }}>
         <Typography component="h1" variant="h2" gutterBottom>
           {title}
